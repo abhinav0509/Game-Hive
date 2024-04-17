2bacd1b78d96aefb38be60c69c70f79bc94fa733
Abhinav Raj <abhinav.raj0509@gmail.com>
2024-04-17 21:46:57 +0530

    solved noimage for games issue

--- src/services/image-optimise.ts
+++ src/services/image-optimise.ts
@@ -1,5 +1,6 @@
+import noImage from "../assets/no-image-placeholder.webp";
 function getOptimisedImage(url: string) {
-  if (!url) return "";
+  if (!url) return noImage;
   const index = url.indexOf("media/") + "media/".length;
   const optimisedUrl = url.slice(0, index) + "crop/600/400/" + url.slice(index);
   return optimisedUrl;
